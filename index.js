8be1a64178527eb36d9b471505815c4d7a0bf4d0
farhan-abdullah <farhan.abdullah94@gmail.com>
2023-05-09 15:26:41 +0200

    68-6

--- index.js
+++ index.js
@@ -31,9 +31,18 @@ async function run() {
 	try {
 		// Connect the client to the server	(optional starting in v4.7)
 		await client.connect();
-
-
-    
+		const coffeeCollection = client.db('coffeeDB').collection('coffee');
+		app.post('/coffee', async (req, res) => {
+			const newCoffee = req.body;
+			const result = await coffeeCollection.insertOne(newCoffee);
+			res.send(result); //client side a send kortesi
+			//
+		});
+		app.get('/coffee', async (req, res) => {
+			const cursor = coffeeCollection.find();
+			const result = await cursor.toArray();
+			res.send(result);
+		});
 		// Send a ping to confirm a successful connection
 		await client.db('admin').command({ ping: 1 });
 		console.log('Pinged your deployment. You successfully connected to MongoDB!');
